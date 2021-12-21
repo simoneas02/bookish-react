28f0be532b1fd1df9e5082afa55f90157d421a27
Simone Amorim <simoneas03@gmail.com>
2021-12-21 09:09:06 -0300

    test: Make the first e2e test pass.

---
+++ cypress/integration/ui/bookish.spec.js
@@ -0,0 +1,6 @@
+describe('Bookisn application', () => {
+  it('Visits the bookish', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('h2[data-test="heading"]').contains('Bookish')
+  })
+})
