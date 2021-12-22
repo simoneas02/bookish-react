1adfdbe685121fa6f44782a943b986b94945bf2d
Simone Amorim <simoneas03@gmail.com>
2021-12-22 09:21:15 -0300

    test: Add BookList acceptance test.

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -3,4 +3,18 @@ describe('Bookisn application', () => {
     cy.visit('http://localhost:3000/')
     cy.get('h2[data-test="heading"]').contains('Bookish')
   })
+
+  it('Shows a book list', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-test="book-list"]').should('exist')
+    cy.get('li.book-item').should('have.length', 2)
+
+    cy.get('li.book-item').should(books => {
+      expect(books).to.have.length(2)
+
+      const titles = [...books].map(book => book.querySelector('h2').innerHTML)
+
+      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+    })
+  })
 })
