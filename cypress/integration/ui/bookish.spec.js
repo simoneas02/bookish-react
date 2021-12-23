afec7114a66de6de1c23dd8ae1e5e09d15d96c65
Simone Amorim <simoneas03@gmail.com>
2021-12-23 09:32:35 -0300

    test: Use fixture data.

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,3 +1,35 @@
+import axios from 'axios'
+
+before(async () => {
+  try {
+    return await axios.delete('http://localhost:8080/books?_cleanup=true')
+  } catch (err) {
+    return err
+  }
+})
+
+afterEach(async () => {
+  try {
+    return await axios.delete('http://localhost:8080/books?_cleanup=true')
+  } catch (err) {
+    return err
+  }
+})
+
+beforeEach(() => {
+  const books = [
+    { name: 'Refactoring', id: 1 },
+    { name: 'Domain-driven design', id: 2 },
+    { name: 'Building Microservices', id: 3 },
+  ]
+
+  return books.map(item =>
+    axios.post('http://localhost:8080/books', item, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  )
+})
+
 describe('Bookisn application', () => {
   it('Visits the bookish', () => {
     cy.visit('http://localhost:3000/')
@@ -7,14 +39,18 @@ describe('Bookisn application', () => {
   it('Shows a book list', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[data-test="book-list"]').should('exist')
-    cy.get('li.book-item').should('have.length', 2)
+    cy.get('li.book-item').should('have.length', 3)
 
     cy.get('li.book-item').should(books => {
-      expect(books).to.have.length(2)
+      expect(books).to.have.length(3)
 
       const titles = [...books].map(book => book.querySelector('h2').innerHTML)
 
-      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+      expect(titles).to.deep.equal([
+        'Refactoring',
+        'Domain-driven design',
+        'Building Microservices',
+      ])
     })
   })
 })
