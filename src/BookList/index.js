042eb2168894b7ef88e65119d8f85091d181bfee
Simone Amorim <simoneas03@gmail.com>
2021-12-23 07:23:31 -0300

    fix: Add key for each child.

--- src/BookList/index.js
+++ src/BookList/index.js
@@ -2,7 +2,7 @@ const BookList = ({ books }) => {
   return (
     <ul data-test="book-list">
       {books.map(({ name }) => (
-        <li className="book-item">
+        <li key={name} className="book-item">
           <h2 className="title">{name}</h2>
         </li>
       ))}
