ceae0fb20630f81c721c335bbac83215264cfb14
Simone Amorim <simoneas03@gmail.com>
2021-12-24 12:51:38 -0300

    refactor: Move book logic into to container.

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import { Typography } from '@material-ui/core'
+import BookListContainer from '../BookListContainer'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+
+      <BookListContainer />
+    </div>
+  )
+}
+
+export default App
