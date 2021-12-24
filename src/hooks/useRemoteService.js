ef8b014cb52591ec851b704b9b5a3de90d0680d6
Simone Amorim <simoneas03@gmail.com>
2021-12-24 12:54:11 -0300

    refactor: Move book logic into to a hook.

---
+++ src/hooks/useRemoteService.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+export const useRemoteService = (initialState, url) => {
+  const [data, setData] = useState(initialState, url)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setError(false)
+      setLoading(true)
+
+      try {
+        const response = await axios.get(url)
+        setData(response.data)
+      } catch (error) {
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return { data, loading, error }
+}
