6ca03ff949d810ba24e072066d9bc561390f55e1
Simone Amorim <simoneas03@gmail.com>
2021-12-26 09:34:41 -0300

    feat: Add Material-UI component to book detail.

--- src/BookDetail/BookDetail.js
+++ src/BookDetail/BookDetail.js
@@ -1,8 +1,40 @@
-const BookDetail = ({ book }) => (
-  <div className="detail">
-    <h2 className="book-title">{book.name}</h2>
-    <p className="book-description">{book.description}</p>
-  </div>
-)
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+} from '@material-ui/core'
+import { useStyles } from '../hooks/useStyles'
+
+const BookDetail = ({ book }) => {
+  const classes = useStyles()
+
+  return (
+    <Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h2"
+            className={classes.name}
+            data-test="book-title"
+          >
+            {book.name}
+          </Typography>
+
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            className={classes.description}
+          >
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
 
 export default BookDetail
