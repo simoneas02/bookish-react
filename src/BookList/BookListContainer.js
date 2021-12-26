14b72cca78afc448a8da8cc96454d1613ecba0e3
Simone Amorim <simoneas03@gmail.com>
2021-12-26 15:20:43 -0300

    feat: Use the Search component.

--- src/BookList/BookListContainer.js
+++ src/BookList/BookListContainer.js
@@ -1,11 +1,26 @@
 import BookList from './BookList'
 import { useRemoteService } from '../hooks/useRemoteService'
+import { useEffect, useState } from 'react'
+import SearchBox from './SearchBox/SearchBox'
 
 const BookListContainer = () => {
   const URL = 'http://localhost:8080/books'
-  const { data, loading, error } = useRemoteService([], URL)
+  const { data, loading, error, setUrl } = useRemoteService(URL, [])
 
-  return <BookList books={data} loading={loading} error={error} />
+  const [term, setTerm] = useState('')
+
+  useEffect(() => {
+    setUrl(`http://localhost:8080/books?q=${term}`)
+  }, [term])
+
+  const onSearch = event => setTerm(event.target.value)
+
+  return (
+    <>
+      <SearchBox term={term} onSearch={onSearch} />
+      <BookList books={data} loading={loading} error={error} />
+    </>
+  )
 }
 
 export default BookListContainer
