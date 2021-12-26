72a90f94d9b81ba31687df0fcf383dacb35fe3e2
Simone Amorim <simoneas03@gmail.com>
2021-12-26 00:41:14 -0300

    feat: Add book deail page.

---
+++ src/BookDetail/BookDetail.js
@@ -0,0 +1,8 @@
+const BookDetail = ({ book }) => (
+  <div className="detail">
+    <h2 className="book-title">{book.name}</h2>
+    <p className="book-description">{book.description}</p>
+  </div>
+)
+
+export default BookDetail
---
+++ src/BookDetail/BookDetailContainer.js
@@ -0,0 +1,16 @@
+import { useParams } from 'react-router-dom'
+
+import { useRemoteService } from '../hooks/useRemoteService'
+
+import BookDetail from './BookDetail'
+
+const BookDetailContainer = () => {
+  const params = useParams()
+  const URL = `http://localhost:8080/books/${params.id}`
+
+  const { data } = useRemoteService({}, URL)
+
+  return <BookDetail book={data} />
+}
+
+export default BookDetailContainer
