2cc25c38f1cc55232e4a1a4607859e40499f9fe4
Simone Amorim <simoneas03@gmail.com>
2021-12-26 19:22:30 -0300

    test: Add integration test for redux store.

---
+++ src/store/store.test.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import * as actions from '../redux/actions/actions'
+import store from './store'
+
+describe('Store', () => {
+  const books = [{ id: 1, name: 'Refactoring' }]
+
+  it('Fetch books from remote', () => {
+    axios.get = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ data: books }))
+
+    return store.dispatch(actions.fetchBooks()).then(() => {
+      const state = store.getState()
+
+      expect(state.books.length).toEqual(1)
+      expect(state.books).toEqual(books)
+    })
+  })
+
+  it('Performs a search', () => {
+    axios.get = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ data: books }))
+
+    store.dispatch(actions.setSearchTerm('domain'))
+
+    return store.dispatch(actions.fetchBooks()).then(() => {
+      const state = store.getState()
+
+      expect(state.term).toEqual('domain')
+      expect(axios.get).toHaveBeenCalledWith(
+        'http://localhost:8080/books?q=domain'
+      )
+    })
+  })
+})
