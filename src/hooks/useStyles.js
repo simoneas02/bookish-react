c44d29663891b4607330557cf6272f9dd4e31b2c
Simone Amorim <simoneas03@gmail.com>
2021-12-26 09:32:23 -0300

    feat: Create a style hook to use material UI style.

---
+++ src/hooks/useStyles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  name: {
+    maxHeight: 30,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  description: {
+    maxHeight: 40,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+}))
