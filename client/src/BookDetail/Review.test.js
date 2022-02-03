eda79e005ea4c2691637b968d0f09de8737c3b5b
Simone Amorim <simoneas03@gmail.com>
2022-02-03 09:23:24 -0800

    test: Add edit review button.

--- client/src/BookDetail/Review.test.js
+++ client/src/BookDetail/Review.test.js
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { renderWithRouter } from '../setupTests'
 
@@ -22,5 +23,25 @@ describe('Review', () => {
     expect(
       screen.getByText('Excellent work, really impressed by your efforts')
     ).toBeInTheDocument()
+  })
+
+  it('should render the Submit button when clicking on the Edit button on each review', async () => {
+    const props = {
+      review: {
+        name: 'Juntao',
+        date: '2018/06/21',
+        content: 'Excellent work, really impressed by your efforts',
+      },
+    }
+
+    renderWithRouter(<Review {...props} />)
+
+    const editButton = await screen.findByRole('button', { name: 'Edit' })
+    expect(editButton).toBeInTheDocument()
+
+    userEvent.click(editButton)
+
+    const submitButton = await screen.findByRole('button', { name: 'Submit' })
+    expect(submitButton).toBeInTheDocument()
   })
 })
