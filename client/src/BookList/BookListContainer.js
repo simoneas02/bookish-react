3862e6125110326b33beb0339550ebfaa741941c
Simone Amorim <simoneas03@gmail.com>
2022-02-07 20:00:50 -0800

    refactor: Improve the search term and add some Grids from material UI.

--- client/src/BookList/BookListContainer.js
+++ client/src/BookList/BookListContainer.js
@@ -1,30 +1,34 @@
 import BookList from './BookList'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import * as actions from '../redux/actions/actions'
-
 import bookListSelector from '../redux/selectors/selector'
+import Grid from '@material-ui/core/Grid/Grid'
+
+import * as actions from '../redux/actions/actions'
 import SearchBox from './SearchBox/SearchBox'
 
 const BookListContainer = () => {
-  const [term] = useState()
+  const [term, setTerm] = useState('')
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(actions.fetchBooks())
+    dispatch(actions.fetchBooks(term))
   }, [term, dispatch])
-
-  const onSearch = event => {
-    dispatch(actions.setSearchTerm(event.target.value))
-    dispatch(actions.fetchBooks())
-  }
 
   const { books, loading, error } = useSelector(bookListSelector)
 
+  const onSearch = event => setTerm(event.target.value)
+
   return (
     <>
-      <SearchBox term={term} onSearch={onSearch} />
-      <BookList books={books} loading={loading} error={error} />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <SearchBox term={term} onSearch={onSearch} />
+        </Grid>
+        <Grid item xs={12}>
+          <BookList books={books} loading={loading} error={error} />
+        </Grid>
+      </Grid>
     </>
   )
 }
