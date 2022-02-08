f831064e3f4c5684375b0870c19bc4dc9c606eaf
Simone Amorim <simoneas03@gmail.com>
2022-02-07 19:56:17 -0800

    refactor: Use new reducer files.

---
+++ client/src/store/store.js
@@ -0,0 +1,28 @@
+import { applyMiddleware, combineReducers, createStore, compose } from 'redux'
+import { routerMiddleware, routerReducer } from 'react-router-redux'
+import thunk from 'redux-thunk'
+import { createBrowserHistory } from 'history'
+
+import books from '../redux/reducers/books'
+import detail from '../redux/reducers/detail'
+import errors from '../redux/reducers/errors'
+
+const rootReducer = combineReducers({
+  routing: routerReducer,
+  books,
+  detail,
+  errors,
+})
+
+export const history = createBrowserHistory()
+
+const initialState = {}
+const middlewares = [thunk, routerMiddleware(history)]
+
+const composeWithDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const composedEnhancers = composeWithDevTools(applyMiddleware(...middlewares))
+const store = createStore(rootReducer, initialState, composedEnhancers)
+
+export default store
